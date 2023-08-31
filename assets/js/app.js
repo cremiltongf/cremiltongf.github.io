0cfccc6f39f581eba8c9c13c4fe93b6f5234b012
Cremilton Fernandes <cremilton@outlook.com>
2023-08-31 14:27:32 -0300

    ajuste de overflow para mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,16 @@ window.addEventListener("load", function(){
         toogleMenu[i].addEventListener("click", menuAction);
     }
 
-
-    let menuContainer = document.querySelector(".mobile");      
+    let overflowScroll = document.querySelector(".content");
+    let menuContainer = document.querySelector(".mobile");   
     function menuAction() {
         if(menuContainer.classList.contains("show-menu")){
             menuContainer.classList.remove("show-menu");
+            overflowScroll.classList.remove("scroll-hidden");
         }
         else {
             menuContainer.classList.add("show-menu");
+            overflowScroll.classList.add("scroll-hidden");
         }
     }
 

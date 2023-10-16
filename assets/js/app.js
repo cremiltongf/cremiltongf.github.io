f7617eef11dfa62b0738342ccd2aeb40500605c1
Cremilton Fernandes <cremilton@outlook.com>
2023-10-16 08:06:56 -0300

    correção de js do menu mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,22 @@
-function mobileMenu() {
+let overflowHidden = document.querySelector("body");
+let menuMobile = document.querySelector(".mobile");
+let toogle = document.querySelectorAll(".toggle");
 
-    let overflowHidden = document.querySelector("body");
-    let menuMobile = document.querySelector(".mobile");
-    let toogle = document.querySelectorAll(".toggle");
-
-    for (let i = 0; i < toogle.length; i++){
-        toogle[i].addEventListener("click", menuAction);
-    }
- 
-    function menuAction() {
-        if(menuMobile.classList.contains("show-menu")){
-            menuMobile.classList.remove("show-menu");
-            overflowHidden.style.overflow = "auto";
-        }
-        else {
-            menuMobile.classList.add("show-menu");
-            overflowHidden.style.overflow = "hidden";
-        }
-    }
-
-    document.addEventListener("keydown", function(event){
-        if(event.key === "Escape") () =>  menuAction();
-    });
+for (let i = 0; i < toogle.length; i++){
+    toogle[i].addEventListener("click", menuAction);
 }
-window.addEventListener("load", mobileMenu());
+
+function menuAction() {
+    if(menuMobile.classList.contains("show-menu")){
+        menuMobile.classList.remove("show-menu");
+        overflowHidden.style.overflow = "auto";
+    }
+    else {
+        menuMobile.classList.add("show-menu");
+        overflowHidden.style.overflow = "hidden";
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Escape") () =>  menuAction();
+});
